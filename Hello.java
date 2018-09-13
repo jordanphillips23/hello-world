c5df6de8d3a6d7c68f10c4915cb38f477e9ad28d
Jordan Phillips <jordan.m.phillips23@gmail.com>
2018-09-13 09:17:43 -0400

    second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, Ap Computer Science!");
+    System.out.println("Hello, AP Computer Science!");
   }
 }
